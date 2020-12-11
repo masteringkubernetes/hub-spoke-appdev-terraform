2f1cd7dbc3d296a09305c5a33760d95f231e46c6
brentmcconnell <emconne@gmail.com>
2020-12-11 16:44:51 -0500

    added terraform to cloud-init script

--- scripts/vm-setup.sh
+++ scripts/vm-setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get install -y apt-transport-https gnupg2
 sudo apt-get install -y kubectl
+sudo apt-get install terraform
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
